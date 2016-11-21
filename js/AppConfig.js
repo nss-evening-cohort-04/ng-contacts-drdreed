2dd4a9c3baa2197653b46883cddb08eae0635805
Dustin Reed <dmreedy1@gmail.com>
2016-11-20 22:00:05 -0600

    finished up routes

--- js/AppConfig.js
+++ js/AppConfig.js
@@ -3,3 +3,24 @@
 app.run((FIREBASE_CONFIG)=>{
   firebase.initializeApp(FIREBASE_CONFIG);
 });
+
+app.config($routeProvider => {
+  $routeProvider
+    .when('/contacts/list', {
+      templateUrl: 'partials/contact-list.html',
+      controller: 'ContactListCtrl'
+    })
+    .when('/contacts/new', {
+      templateUrl: 'partials/contact-new.html',
+      controller: 'ContactNewCtrl'
+    })
+    .when('/contacts/details/:id', {
+      templateUrl: 'partials/contact-details.html',
+      controller: 'ContactDetailsCtrl'
+    })
+    .when('/contacts/edit/:id', {
+      templateUrl: 'partials/contact-new.html',
+      controller: 'ContactEditCtrl'
+    })
+    .otherwise('/contacts/list');
+});
